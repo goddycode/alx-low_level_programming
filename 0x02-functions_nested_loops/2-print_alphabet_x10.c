362790d04fcb902fcdf78125d0ec79d48ab7fda6
goddycode <odhisgoddy24@gmail.com>
2022-12-14 10:30:19 +0000

    Task 2 correction commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@ void print_alphabet_x10(void)
 {
 	int i, j = 0;
 
-	while (i <= 9)
+	while (j <= 9)
 	{
 		for (i = 97; i <= 122; i++)
 		{
 			_putchar(i);
 		}
-	i++;
+	j++;
 	_putchar('\n');
 	}
 }
