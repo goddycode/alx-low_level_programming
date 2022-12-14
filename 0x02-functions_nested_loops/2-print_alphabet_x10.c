b39550f92f204492513ea5a8cafbb15baa276b73
goddycode <odhisgoddy24@gmail.com>
2022-12-14 08:52:34 +0000

    0x02-functions_nested_loops task 0 - 2 commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -  function that prints 10 times the alphabet, in lowercase
+ * Return: Always return 0
+ */
+void print_alphabet_x10(void)
+{
+	int i, j = 0;
+	while (i <=9)
+	{
+		for (i = 97; i <=122; i++)
+		{
+			_putchar(i);
+		}
+	i++;
+	_putchar("\n");
+	}
+}
+
