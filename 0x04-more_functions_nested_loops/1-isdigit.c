b89d04abc234341c9cdb4f50b56a784bb260bec8
goddycode <odhisgoddy24@gmail.com>
2022-12-16 00:04:33 -0800

    Correcting task 1 error

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,9 @@
 #include "main.h"
-
-
-
 /**
-
-   * _isdigit - checks for a digit (0 through 9)
-
-    * @digit: int 0 -9 to be checked
-
-     * Return: 1 if num is a digit, 0 otherwise
-
-      */
+* _isdigit - checks for a digit (0 through 9)
+* @digit: int 0 -9 to be checked
+* Return: 1 if num is a digit, 0 otherwise
+*/
 
 int _isdigit(int digit)
 
