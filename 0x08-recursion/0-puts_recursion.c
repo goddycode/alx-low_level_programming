334067171b2a1aa0ea39466c2d322af5ed1084b8
godycode <odhisgoddy24@gmail.com>
2023-01-05 14:03:25 +0000

    0-puts_recursion.c file corrective commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,19 +2,16 @@
 #include <stdio.h>
 
 /**
- * _puts_recursion - function to print string 
+ *_puts_recursion - function to print string
  *@s: Parameter
  * Return: Always 0.
  */
 void _puts_recursion(char *s)
 {
-        int i;
 
-	for(i = 0; s[i != '\0'; i++])
+	if (*s != '\0')
 	{
-		_putchar(s[i]);
-		_putchar('\n);	
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-
 }
-
