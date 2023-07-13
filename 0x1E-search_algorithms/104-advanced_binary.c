4db89df4440ff8af7ea829e328367e110835f59d
vagrant <vagrant@ubuntu2004.localdomain>
2023-07-13 05:59:09 +0000

    Adding given files t0 '0x1E-search_algorithms' commit#3

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -11,30 +11,28 @@
  * Return: index of the number
  */
 
-int binary_search(int *array, size_t low, size_t high, int value) {
-    if (array == NULL || low > high) {
-        return -1;
-    }
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
+{
+	size_t i;
 
-    size_t mid = low + (high - low) / 2;
+	if (right < left)
+		return (-1);
 
-    printf("Searching in array: ");
-    for (size_t i = low; i <= high; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
 
-    if (array[mid] == value) {
-        return mid;
-    } else if (array[mid] < value) {
-        return binary_search(array, mid + 1, high, value);
-    } else {
-        return binary_search(array, low, mid - 1, value);
-    }
+	i = left + (right - left) / 2;
+	if (array[i] == value && (i == left || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (advanced_binary_recursive(array, left, i, value));
+	return (advanced_binary_recursive(array, i + 1, right, value));
 }
 
-/**
- * advanced_binary - calls to rec_search to return
+
+/** advanced_binary - calls to rec_search to return
  * the index of the number
  *
  * @array: input array
@@ -42,6 +40,11 @@ int binary_search(int *array, size_t low, size_t high, int value) {
  * @value: value to search in
  * Return: index of the number
  */
-int advanced_binary(int *array, size_t size, int value) {
-    return binary_search(array, 0, size - 1, value);
-}
+
+int advanced_binary(int *array, size_t size, int value)
+	{
+		if (array == NULL || size == 0)
+			return (-1);
+
+		return (advanced_binary_recursive(array, 0, size - 1, value));
+	}
