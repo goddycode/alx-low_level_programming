da64afe5f0b6794ee8925bb324eb21232ae78d28
vagrant <vagrant@ubuntu2004.localdomain>
2023-08-23 11:18:34 +0000

    month 6-8 tasks for alx course defrement commit #3

---
+++ 0x00-hello_world/7-betty_test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - this is main function
+* Descriptin : print hello world
+* Return: Always 0
+*/
+
+int main(void)
+{
+	printf("Hello word");
+}
